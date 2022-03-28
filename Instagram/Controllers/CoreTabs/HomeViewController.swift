98de2f1af1f3a84ac70060746281253b6eb24613
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-03-28 22:44:30 +0300

    Initial commit

--- Instagram/Controllers/CoreTabs/HomeViewController.swift
+++ Instagram/Controllers/CoreTabs/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Instagram
 //
 //  Created by Николай Никитин on 28.03.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
